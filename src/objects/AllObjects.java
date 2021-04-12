0d7bb320405c9e8db879d6f3f40f37626cc7b572
Afghanistanjan <hak9803@gmail.com>
2021-04-12 10:43:36 -0400

    this is initial pushing files from pc to remot repositry.

---
+++ src/objects/AllObjects.java
@@ -0,0 +1,20 @@
+package objects;
+
+public class AllObjects {
+
+//1.    Actions action = new Actions(driver);                                          //mouse movement or doubleclick…
+//2.	WebDriver driver = new ChromeDriver();                                         //
+//3.	Select select = new Select();                                                  //use for dropdown
+//4.	WebDriverWait wait = new WebDriverWait(driver, 15);                            //it use for explicite wait
+//5.	JavascriptExecutor js = ((JavascriptExecutor)driver);
+//6.	Alert alert = driver.SwitchTo().alert();                                       //it use for alert or papup.
+//7.	Date date = new Date();                                                        //it use for dating of screenshot
+//8.	File file = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+//9.	private static Connection connection;
+//10.	private static Statement statement;
+//11.	private static ResultSet resultSet;
+//12.	
+
+	
+	
+}
